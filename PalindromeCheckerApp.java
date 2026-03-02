4a0a372330ec9d6bfd0fcee1dda7a441208e076a
Venkateshwaran-G-M <gurumoorthivenkateshwaran@gmail.com>
2026-03-02 14:19:24 +0530

    UC5

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,74 +1,27 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
-    private static final String APP_NAME = "Palindrome Checker Management System";
-    private static final String APP_VERSION = "1.0";
-
     public static void main(String[] args) {
-        displayWelcomeMessage();
-
-        checkHardcodedPalindrome();
-
-        checkPalindromeUsingReverse();
-
-        checkPalindromeUsingCharArray();
-
-        System.out.println("System initialized successfully.");
-    }
-    private static void displayWelcomeMessage() {
-        System.out.println("Welcome to the " + APP_NAME);
-        System.out.println("Version : " + APP_VERSION);
-    }
-    private static void checkHardcodedPalindrome() {
-        String input = "madam";
-        boolean result = isPalindrome(input);
-        System.out.println("Input text: " + input);
-        System.out.println("Is it a palindrome? : " + result);
-    }
-    private static boolean isPalindrome(String input) {
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                return false;
-            }
+        Scanner sc = new Scanner(System.in);
+        Stack<Character> stack = new Stack<>();
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
         }
-        return true;
-    }
-    private static void checkPalindromeUsingReverse() {
-        String input = "madam";
-        boolean result = isPalindromeByReverse(input);
-        System.out.println("Input text: " + input);
-        System.out.println("Reversed text: " + reverseString(input));
-        System.out.println("Is it a palindrome? : " + result);
-    }
-    private static String reverseString(String input) {
-        String reversed = "";
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
-        }
-        return reversed;
-    }
-    private static boolean isPalindromeByReverse(String input) {
-        String reversed = reverseString(input);
-        return input.equals(reversed);
-    }
-    private static void checkPalindromeUsingCharArray() {
-        String input = "radar";
-        boolean result = isPalindromeUsingCharArray(input);
-        System.out.println("Input: " + input);
-        System.out.println("Is Palindrome? : " + result);
-    }
-    private static boolean isPalindromeUsingCharArray(String input) {
-        char[] chars = input.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
         boolean isPalindrome = true;
-
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
-        return isPalindrome;
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+        sc.close();
     }
 }
